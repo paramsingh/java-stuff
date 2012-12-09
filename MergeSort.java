74a20e5e3222b7f1928b71f684d75a2eedd0695e
Param <paramsingh258@gmail.com>
2012-12-09 14:18:45 +0530

    Wrote main() in MergeSort.java
    The main() function now inputs an array and prints it.
    Still need to write the merge function so that sorting works.

--- MergeSort.java
+++ MergeSort.java
@@ -10,4 +10,19 @@ class MergeSort{
         }
     }
     //Merge function needs to be written
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        int size = in.nextInt();
+        int a[] = new int[size];
+        int i;
+        System.out.println("Enter the array");
+        for(i=0; i<size; i++){
+            a[i] = in.nextInt();
+        }
+        System.out.println("Original array is: ");
+        for(i: a){
+            System.out.println(i);
+        }
+    }
 }
