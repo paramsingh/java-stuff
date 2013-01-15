5de80b98e28ee18df0704041c2860d92deadb148
Param <paramsingh258@gmail.com>
2013-01-15 17:20:57 +0530

    Add function to reverse word wise to Reverse.java

--- Practical Practice - 15-01-13/Reverse.java
+++ Practical Practice - 15-01-13/Reverse.java
@@ -3,8 +3,11 @@ public class Reverse {
     public static void main(String[] args){
         Scanner in = new Scanner( System.in );
         System.out.println("Enter a sentence.");
-        String userinput = in.nextLine();
-        System.out.println(reverseAtPlace(userinput));
+        String userInput = in.nextLine();
+        System.out.println("When reversed at own place : ");    
+        System.out.println(reverseAtPlace(userInput));
+        System.out.println("When reversed wordwise : ");
+        System.out.println(reverseWordWise(userInput));
     }
     static String reverseText(String s){
         /**
@@ -23,7 +26,7 @@ public class Reverse {
         //Split the string into its words
         String[] words = input.split(" ");
         
-        /**String to contain the reversed word*/
+        //String to contain the reversed input.
         String reversed = "";
         
         for(String word : words){
@@ -32,6 +35,34 @@ public class Reverse {
             reversed += reverseText(word) + " ";
         }
         
+        //returning the string which contained the reversed string
+        return reversed;
+    }
+    static String reverseWordWise(String sentence){
+        /**
+         * reverse the sentence word wise and return it
+         * For Example:
+         * Input : Hello this is dog
+         * Output: dog is this Hello
+         */
+        
+        //Split the string into its words
+        String[] words = sentence.split(" ");
+         
+        //String to contain the reversed input.
+        String reversed = "";
+         
+        //Number of words in the sentence
+        int numWords = words.length;
+         
+        //looping variable
+        int i;
+         
+        for(i = numWords - 1; i>=0; i--){
+            //looping through the array in reverse order & adding each
+            //word to the string "reversed"
+            reversed = reversed + words[i] + " ";
+        }
         return reversed;
     }
 }
