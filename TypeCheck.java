9ddd2ab90ca2cd7af06e5c5119271efd10dca6c4
Param <paramsingh258@gmail.com>
2013-01-15 20:58:35 +0530

    Add a line so that date checks for "/" also

--- TypeCheck.java
+++ TypeCheck.java
@@ -60,7 +60,7 @@ class TypeCheck {
             return false;
         for( int i =0; i<s.length(); i++){
             char ch = s.charAt(i);
-            if( ch!='-' && i==2 && i==5 )
+            if( ch!='-'&& ch!='/' && i==2 && i==5 )
                 return false;
             if( i!=2 && i!=5 && !Character.isDigit(ch) )
                 return false;
