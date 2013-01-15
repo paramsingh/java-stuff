e925c3f6859ad30f84131021fdf67bd22b6113a5
Param <paramsingh258@gmail.com>
2013-01-15 20:28:11 +0530

    Write a program to solve a daily programmer prob.

---
+++ TypeCheck.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+class TypeCheck {
+    /*
+     * The Problem :
+     * http://www.reddit.com/r/dailyprogrammer/comments/13hmz3/11202012_challenge_113_easy_stringtype_checking/
+     */ 
+    public static void main(String[] args) {
+        Scanner in = new Scanner( System.in );
+        while(true){
+            System.out.println("Enter the string.");
+            String input = in.nextLine();
+            printType(input);
+            System.out.print("Try again(y/n): ");
+            if(!in.nextLine().equalsIgnoreCase("y"))
+                break;
+        }
+    }
+    public static void printType( String s ){
+        if( isInt(s) )
+            System.out.println("Integer");
+        else if( isFloat(s) )
+            System.out.println("Float");
+        else
+            System.out.println("Text");
+    }
+    public static boolean isInt(String s){
+        if(s.contains("."))
+            return false;
+        else {
+            for(int i=0; i<s.length(); i++){
+                char ch = s.charAt(i);
+                if( !Character.isDigit(ch) ){
+                    if( (ch=='+'||ch=='-') && i==0)
+                        continue;
+                    else
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+    public static boolean isFloat(String s){
+        if( !s.contains('.'+"") )
+            return false;
+        for( int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(!Character.isDigit(ch) && ch!='.'){
+                if( (ch=='+'||ch=='-') && i==0 )
+                    continue;
+                else
+                    return false;
+            }
+        }
+        return true;
+    }
+    //need to write a check function for checking dates
+}
